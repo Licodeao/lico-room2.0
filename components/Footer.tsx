efcdfee5fc70b7c5ba82ceaa099762db13f2c099
Licodeao <1019805199@qq.com>
2023-02-11 22:19:33 +0800

    deploy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,16 +10,16 @@ const Footer: FC<IProps> = () => {
     <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 text-center">
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb words</p>
+        <p>How LicoRoom works</p>
         <p>Newsroom</p>
         <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p>Licodeao Plus</p>
+        <p>Licodeao Luxe</p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">COMMUNITY</h5>
         <p>Accessibility</p>
-        <p>This is nor real site</p>
+        <p>This is not real site</p>
         <p>Its a pretty awesome clone</p>
         <p>Referrals accepted</p>
         <p>Licodeao</p>
@@ -36,7 +36,7 @@ const Footer: FC<IProps> = () => {
         <h5 className="font-bold">SUPPORT</h5>
         <p>Help Centre</p>
         <p>Trust & Safety</p>
-        <p>Say Hi Youtube</p>
+        <p>Say Hi Licodeao</p>
         <p>Easter Eggs</p>
         <p>For the win</p>
       </div>
